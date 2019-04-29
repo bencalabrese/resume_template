1cbf854f9241459c81c53c40c2fa3ad822b98e64
Ben Calabrese <bencalabrese@gmail.com>
2019-04-29 16:44:13 -0700

    parameterize skills

--- src/templateParams.js
+++ src/templateParams.js
@@ -1,3 +1,21 @@
 export default {
-  name: "Ben Calabrese"
+  name: "Ben Calabrese",
+  skills: [
+    { name: "Dart" },
+    { name: "AngularDart" },
+    { name: "Typescript" },
+    { name: "Python" },
+    { name: "SQL" },
+    { name: "Ruby" },
+    { name: "Git" },
+    { name: "Rails" },
+    { name: "HTML" },
+    { name: "Javascript" },
+    { name: "CSS" },
+    { name: "jQuery" },
+    { name: "Flux" },
+    { name: "React" },
+    { name: "C" },
+    { name: "Sass" }
+  ]
 };
