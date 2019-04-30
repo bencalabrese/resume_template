ebdbafccc0d8afc9b3d73520a6c693a96adf0321
Ben Calabrese <bencalabrese@gmail.com>
2019-04-30 09:29:50 -0700

    Add experience

--- src/templateParams.js
+++ src/templateParams.js
@@ -24,6 +24,17 @@ export default {
     { name: "C" },
     { name: "Sass" }
   ],
+  experiences: [
+    {
+      name: "Google",
+      descriptors: ["Software Engineer", "Sept. 2016–Present"],
+      bullets: [
+        "Implemented HTML5 canvas rendering with controls for" +
+          "pan, zoom, and adjusting cycle speed",
+        "Bound UI events in jQuery for user manipulation of grid"
+      ]
+    }
+  ],
   projects: [
     {
       name: "Constellations",
@@ -34,7 +45,7 @@ export default {
           url: "https://github.com/bencalabrese/Constellations"
         }
       ],
-      description: "Conway's Game of Life simulator built in Javascript",
+      descriptors: ["Conway's Game of Life simulator built in Javascript"],
       bullets: [
         "Implemented HTML5 canvas rendering with controls for" +
           "pan, zoom, and adjusting cycle speed",
@@ -46,7 +57,7 @@ export default {
       links: [
         { name: "GitHub", url: "https://github.com/bencalabrese/pickup_tix/" }
       ],
-      description: "Fullstack event ticketing app built in Rails and React",
+      descriptors: ["Fullstack event ticketing app built in Rails and React"],
       bullets: [
         "Build a dynamic show filter using ActiveRecord to run a single " +
           "efficient SQL query across multiple criteria",
@@ -59,7 +70,7 @@ export default {
       links: [
         { name: "GitHub", url: "https://github.com/bencalabrese/Snails/" }
       ],
-      description: "Lightweight Rails replica built in Ruby",
+      descriptors: ["Lightweight Rails replica built in Ruby"],
       bullets: [
         "Hand-rolled ORM with built-in SQL query methods",
         "Constructed router, views, and controllers to create" +
