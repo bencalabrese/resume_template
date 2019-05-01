670c2c12f9f4ef6633307cc04d56abad52c55e64
Ben Calabrese <bencalabrese@gmail.com>
2019-05-01 06:40:44 -0700

    try for collapsing, not there yet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 require("./styles.scss");
+
+const contact = document.querySelector(".contact");
+const skills = document.querySelector(".skills");
+const education = document.querySelector(".education");
+
+setTimeout(() => {
+  contact.classList.add("collapsed");
+  skills.classList.add("expanded");
+  education.classList.add("collapsed");
+}, 1000);
