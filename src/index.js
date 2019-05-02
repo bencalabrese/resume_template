6dc79f97060c51bebdc19100f5094f59e6129cf1
Ben Calabrese <bencalabrese@gmail.com>
2019-05-01 17:21:56 -0700

    collapsing works well, just needs fine tuning

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ const contact = document.querySelector(".contact");
 const skills = document.querySelector(".skills");
 const education = document.querySelector(".education");
 
+document.querySelectorAll(".collapsible").forEach(element => {
+  const originalHeight = element.scrollHeight;
+  element.style.flexBasis = originalHeight;
+});
+
 setTimeout(() => {
-  contact.classList.add("collapsed");
-  skills.classList.add("expanded");
-  education.classList.add("collapsed");
+  collapse(contact);
+  collapse(education);
 }, 1000);
+
+function collapse(element) {
+  element.style.flexBasis = 0;
+  element.style.flexGrow = 0;
+
+  if (element === contact) {
+    element.style.flexBasis = "10px";
+  }
+}
