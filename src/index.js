3ddb3de1086b5710e14f645776046d5ff4d8170a
Ben Calabrese <bencalabrese@gmail.com>
2019-05-01 18:08:45 -0700

    cleanly toggling contact data with heading

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ const education = document.querySelector(".education");
 
 const collapsibles = Array.from(document.querySelectorAll(".collapsible"));
 
-var headingHeight;
-
-collapsibles
-  .filter(element => element.matches(".has-initially-collapsed-heading"))
-  .forEach(element => {
-    const heading = element.querySelector(".initially-collapsed-heading");
-    headingHeight = heading.scrollHeight;
-  });
+let [
+  contactHeadingHeight,
+  skillsHeadingHeight,
+  educationHeadingHeight
+] = collapsibles.map(element => {
+  const heading = element.querySelector("h3");
+  console.log(heading.scrollHeight);
+  return heading.scrollHeight;
+});
 
 collapsibles.forEach(element => {
   const originalHeight = element.scrollHeight;
@@ -26,10 +27,12 @@ setTimeout(() => {
 }, 1000);
 
 function collapse(element) {
+  element.classList.add("collapsed");
   if (element === contact) {
-    element.style.flexBasis = `${headingHeight}px`;
-  } else {
-    element.style.flexBasis = 0;
+    console.log(contactHeadingHeight);
+    element.style.flexBasis = `${contactHeadingHeight}px`;
+  } else if (element === education) {
+    element.style.flexBasis = `${educationHeadingHeight}px`;
   }
   element.style.flexGrow = 0;
 }
