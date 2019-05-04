5dded7b73946951636830ebf77bcba2105bd84e6
Ben Calabrese <bencalabrese@gmail.com>
2019-05-04 14:29:32 -0700

    LHS to transitions so they move back easily

--- src/actions/profile.ts
+++ src/actions/profile.ts
@@ -3,6 +3,12 @@ import Stretchable from "./stretchable";
 
 export default class Profile {
   private readonly main: HTMLElement = document.querySelector("main");
+  private readonly leftColumn: HTMLElement = document.querySelector(
+    ".left-column"
+  );
+  private readonly rightColumn: HTMLElement = document.querySelector(
+    ".right-column"
+  );
   readonly stretchables: Stretchable[];
 
   constructor() {
@@ -15,11 +21,24 @@ export default class Profile {
   }
 
   private minimize(): void {
-    this.main.classList.remove("expanded-profile");
+    this.leftColumn.classList.remove("grow-horizontally");
+    this.rightColumn.classList.remove("reduce-horizontally");
+
+    // setTimeout is more consistent than transitionend
+    setTimeout(() => {
+      this.main.classList.remove("expanded-profile");
+      this.stretchables.forEach(stretchable => stretchable.normalize());
+    }, 1000);
   }
 
   maximize(): void {
     this.main.classList.add("expanded-profile");
+
+    // setTimeout is more consistent than transitionend
+    setTimeout(() => {
+      this.leftColumn.classList.add("grow-horizontally");
+      this.rightColumn.classList.add("reduce-horizontally");
+    }, 1000);
   }
 
   expand(target: Stretchable): void {
@@ -30,7 +49,6 @@ export default class Profile {
   }
 
   normalize(): void {
-    this.stretchables.forEach(stretchable => stretchable.normalize());
     this.minimize();
   }
 }
