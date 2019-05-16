85d95c61b7e3fc5219bfd335ecf9c367abd14d0b
Ben Calabrese <bencalabrese@gmail.com>
2019-05-15 20:28:30 -0700

    clean up cruft in ts

--- src/actions/coordinator.ts
+++ src/actions/coordinator.ts
@@ -1,23 +1,11 @@
-import History from "./history";
 import Profile from "./profile";
 import Stretchable from "./stretchable";
 import { throttle } from "lodash";
 
 export default class Coordinator {
   private readonly profile = new Profile();
-  private readonly history = new History();
-  readonly main: HTMLElement = document.querySelector("main");
 
   constructor() {
-    // Allow flexbox to figure out the initial way to lay out the enclosures
-    // but ensure they don't shrink height after that.
-    document
-      .querySelectorAll(".enclosure")
-      .forEach((enclosure: HTMLElement) => {
-        const originalHeight = `${enclosure.scrollHeight}px`;
-        enclosure.style.minHeight = originalHeight;
-      });
-
     this.profile.stretchables.forEach(this.attachStretchableHandler.bind(this));
 
     this.attachGlobalHandler();
@@ -36,7 +24,6 @@ export default class Coordinator {
     const handleClick = () => {
       this.profile.expand(stretchable);
       this.profile.maximize();
-      this.history.lower();
     };
     stretchable.overlay.addEventListener(
       "click",
@@ -46,6 +33,5 @@ export default class Coordinator {
 
   normalize(): void {
     this.profile.normalize();
-    this.history.normalize();
   }
 }
