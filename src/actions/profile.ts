446a1432c4aa9b0c65950ca1b31c3e66ff094ed3
Ben Calabrese <bencalabrese@gmail.com>
2019-05-25 08:58:05 -0700

    expansion is better, still very messed up inside

--- src/actions/profile.ts
+++ src/actions/profile.ts
@@ -4,9 +4,5 @@ import Column from "./column";
 export default class Profile extends Column {
   constructor() {
     super("profile", "expanded-profile");
-
-    fixWidth(".app-academy .info");
-    fixWidth(".sf-state .info");
-    fixWidth(".about .info");
   }
 }
