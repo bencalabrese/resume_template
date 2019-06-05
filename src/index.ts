41a6e86d94eedac03a4c314febe48bb8b0b758e2
Ben Calabrese <bencalabrese@google.com>
2019-06-05 08:32:18 -0700

    fix dom slowdown when coming from external link

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,21 @@ require("./styles.scss");
 import Coordinator from "./actions/coordinator";
 import Instructions from "./actions/instructions";
 
-new Coordinator();
-const interactionInstructions = new Instructions(".left .instructions");
-const printInstructions = new Instructions(".right .instructions");
+window.addEventListener("DOMContentLoaded", () => {
+  // The dom needs a little time to adjust before we can start manually setting
+  // flexBasis in the stretchables.
+  setTimeout(() => {
+    new Coordinator();
+  }, 500);
 
-setTimeout(() => {
-  interactionInstructions.in();
-}, 2000);
+  const interactionInstructions = new Instructions(".left .instructions");
+  const printInstructions = new Instructions(".right .instructions");
 
-setTimeout(() => {
-  printInstructions.in();
-}, 2700);
+  setTimeout(() => {
+    interactionInstructions.in();
+  }, 2000);
+
+  setTimeout(() => {
+    printInstructions.in();
+  }, 2700);
+});
