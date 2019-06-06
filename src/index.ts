af1aee8e93844e6635dc92b0036332022039fd26
Ben Calabrese <bencalabrese@google.com>
2019-06-06 08:54:16 -0700

    fix the weird page load issue once and for all

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,16 @@ require("./styles.scss");
 import Coordinator from "./actions/coordinator";
 import Instructions from "./actions/instructions";
 
+// Fixes odd issue where elements transition on page load. This issue only
+// occurs intermittently and seemingly only from certain types of links.
+//
+// We solve here by enforcing 0 transition time until a delay after page load.
+// Not an ideal solution, but dev time is better spent on other problems.
+const body: HTMLElement = document.querySelector("body");
+setTimeout(() => {
+  body.classList.remove("hidden");
+}, 300);
+
 new Coordinator();
 
 const interactionInstructions = new Instructions(".left .instructions");
