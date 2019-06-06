2ced4c74103119c1499fc83b00413634ab4cf278
Ben Calabrese <bencalabrese@google.com>
2019-06-06 07:30:56 -0700

    return to original setup for svgs

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,15 @@ require("./styles.scss");
 import Coordinator from "./actions/coordinator";
 import Instructions from "./actions/instructions";
 
-window.addEventListener("DOMContentLoaded", () => {
-  // The dom needs a little time to adjust before we can start manually setting
-  // flexBasis in the stretchables.
-  setTimeout(() => {
-    new Coordinator();
-  }, 500);
+new Coordinator();
 
-  const interactionInstructions = new Instructions(".left .instructions");
-  const printInstructions = new Instructions(".right .instructions");
+const interactionInstructions = new Instructions(".left .instructions");
+const printInstructions = new Instructions(".right .instructions");
 
-  setTimeout(() => {
-    interactionInstructions.in();
-  }, 2000);
+setTimeout(() => {
+  interactionInstructions.in();
+}, 2000);
 
-  setTimeout(() => {
-    printInstructions.in();
-  }, 2700);
-});
+setTimeout(() => {
+  printInstructions.in();
+}, 2700);
