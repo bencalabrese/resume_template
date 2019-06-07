2b8371fdbdcfaaeeabb17dd642cd68bfca49acb1
Ben Calabrese <bencalabrese@google.com>
2019-06-07 07:38:07 -0700

    update docs for chrome bug

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,12 @@ import Instructions from "./actions/instructions";
 //
 // We solve here by enforcing 0 transition time until a delay after page load.
 // Not an ideal solution, but dev time is better spent on other problems.
+//
+// This is a known Chrome bug tracked in
+// https://bugs.chromium.org/p/chromium/issues/detail?id=332189
 const body: HTMLElement = document.querySelector("body");
 setTimeout(() => {
-  body.classList.remove("hidden");
+  body.classList.remove("force-no-transition");
 }, 300);
 
 new Coordinator();
